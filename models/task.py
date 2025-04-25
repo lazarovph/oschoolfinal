9dc612972b8e0ce7d86c150ce66ce16e2e73b93e
lazarovph <90379778+lazarovph@users.noreply.github.com>
2025-04-25 13:54:48 +0300

    Create task.py

---
+++ models/task.py
@@ -0,0 +1,7 @@
+from app import db
+
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(150), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
